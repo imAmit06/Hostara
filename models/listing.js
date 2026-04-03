746f61333cbf8342b33e0e3e14c069903fe4a1a8
imAmit06 <patilamit111006@gmail.com>
2026-04-03 23:57:35 +0530

    Initial Commit

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://plus.unsplash.com/premium_photo-1682309827763-4aa187e27a96?q=80&w=1212&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://plus.unsplash.com/premium_photo-1682309827763-4aa187e27a96?q=80&w=1212&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("listing", listingSchema);
+module.exports = Listing;
